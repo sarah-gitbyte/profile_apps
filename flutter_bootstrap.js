c315994aa45d4ae7d5627bcc9b095845eb232558
sarah-gitbyte <sarahamani0620@gmail.com>
2026-05-08 18:20:21 +0800

    Deploy web version

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3333591127" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "949267628" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
